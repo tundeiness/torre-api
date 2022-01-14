88318282132c8e8f3cf0dddb16d2cb1662b06d55
tundeiness <tundeiness@yahoo.co.uk>
2022-01-14 16:06:19 +0100

    remake controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,10 @@
-class Api::V1::UserController < Api::V1::ApiController
+class Api::V1::UsersController < Api::V1::ApiController
  
   def index
     binding.pry
     res = RestClient.get("https://torre.bio/api/bios/john")
     @user = JSON.parse(res)
+    render json: @user
 
     # @books = params[:category] ? Book.categorized(params[:category]) : Book.all
     #  render json: @books

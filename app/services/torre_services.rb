0682f946ccf8ba2536ff307561bf8b7b358b9e0c
tundeiness <tundeiness@yahoo.co.uk>
2022-01-17 12:48:01 +0100

    add services directory && setup torre_services.rb

---
+++ app/services/torre_services.rb
@@ -0,0 +1,11 @@
+class TorreService 
+  def get_person_by_username(username)
+    response = Faraday.get("https://torre.bio/api/bios/#{username}")
+    JSON.parse(response)
+  end
+
+  def get_opportunity_by_id(id)
+    response = Faraday.get("https://torre.co/api/suite/opportunities/#{id}")
+    JSON.parse(response)
+  end
+end

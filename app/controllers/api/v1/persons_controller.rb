459dc356251e32964ed6605326693fbcc8735efc
tundeiness <tundeiness@yahoo.co.uk>
2022-01-19 15:51:30 +0100

    complete recieve parameters from the url

--- app/controllers/api/v1/persons_controller.rb
+++ app/controllers/api/v1/persons_controller.rb
@@ -1,13 +1,29 @@
 module Api
   module V1
     class PersonsController < ApplicationController
+      before_action :person_params
+
       def index
-        @response = RestClient.get 'https://torre.bio/api/bios/john', { content_type: :json, accept: :json }
+        @response = RestClient.get `https://torre.bio/api/bios/#{params[:id]}`, { content_type: :json, accept: :json }
 
         @person_details = JSON.parse(@response.body)["person"]["flags"]
         render json: @person_details, status: :ok
       end
+
+      def show 
+        #binding.pry
+        name = params[:username]
+        @response = RestClient.get 'https://torre.bio/api/bios/' + name , { content_type: :json, accept: :json }
+
+        @person_details = JSON.parse(@response.body)["person"]["flags"]
+        render json: @person_details, status: :ok
+      end
+
+      private
+
+      def person_params
+        params.permit(:username)
+      end
     end
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,16 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :persons
+      resources :persons, only: :show, param: :username
     end
   end
-  # get 'persons#person'
+
+
+  #get 'persons/:id', to: 'persons#show'
+  get ':persons', to: 'persons#show', as: :person
+  # resources :persons
+  
+  #match 'persons/:id' => 'persons#show', :via => :get
+  #match '/*username', to: redirect { |params| "/api/v1/#{params[:username]}" }
+  # match '/username', to: redirect { |params| "/api/v1/#{params[:username]}" }, :via => [:get, :post]
 end
-# , only: %i[person]

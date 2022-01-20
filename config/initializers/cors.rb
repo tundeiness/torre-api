bca83ed712f3abcf9e2edeeebb472235ab4f51f7
tundeiness <tundeiness@yahoo.co.uk>
2022-01-20 15:34:31 +0100

    edit cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-   # origins "*"
-    origins "http://localhost:3001/"
+    origins "*"
+   # origins "http://localhost:3000/"
 
     resource "/api/v1/*",
       headers: :any,

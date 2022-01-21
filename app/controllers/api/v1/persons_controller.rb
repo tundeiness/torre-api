c8ca931b6e7a42353c9565c01c0e347445b18f6b
tundeiness <tundeiness@yahoo.co.uk>
2022-01-21 09:29:03 +0100

    edit controller

--- app/controllers/api/v1/persons_controller.rb
+++ app/controllers/api/v1/persons_controller.rb
@@ -3,15 +3,7 @@ module Api
     class PersonsController < ApplicationController
       before_action :person_params
 
-      def index
-        @response = RestClient.get `https://torre.bio/api/bios/#{params[:id]}`, { content_type: :json, accept: :json }
-
-        @person_details = JSON.parse(@response.body)["person"]["flags"]
-        render json: @person_details, status: :ok
-      end
-
-      def show 
-        #binding.pry
+      def show
         name = params[:username]
         response = RestClient.get 'https://torre.bio/api/bios/' + name , { content_type: :json, accept: :json }
 
